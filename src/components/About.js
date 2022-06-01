ca4520d2c974e6e0c13c5df7b8499fa70796d590
Lian <biakhong@gmail.com>
2022-05-31 22:51:19 -0400

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p> This code is written by Lian Hong, the best Software Developer alive.
+    </p>
+    <img src={image} alt="I made this"></img>
+
+    </div>;
 }
 
 export default About;
